7d2784f37c971621e346f2eb40d52972f4b7fd18
AMenzel <info@amenzel.de>
2025-04-09 15:59:41 +0200

    Update MSTeams.js

--- src/MSTeams.js
+++ src/MSTeams.js
@@ -279,9 +279,12 @@ class MSTeams {
   async notify(url, payload) {
     const client = new IncomingWebhook(url);
     const response = await client.send(payload);
-    core.info(response.text)
+    core.info("responsetext'"+response.text+"'")
+    core.info(!response.text)
+    core.info(response.text != "")   
+    core.info(!response.text && response.text != "")
     if (!response.text && response.text != "") {
-      throw new Error('Failed to send notification to Microsoft Teams.\n' + 'Response:\n' + JSON.stringify(response, null, 2));
+      throw new Error('Failed to send notification to Microsoft Teams.\n' + '123Response'+response.text +':\n' + JSON.stringify(response, null, 2));
     }
   }
 }
